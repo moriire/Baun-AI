485bd589ac11907f083b7359a2a0e712f8cdb149
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-09-10 16:40:52 +0100

    latest

--- src/App.vue
+++ src/App.vue
@@ -30,9 +30,9 @@ import {
 			</div>
 		</div>
 			<RouterView />
-		<footer class="text-center text-light p-3">
+		<!--footer class="text-center text-light p-3">
 			<p>Responses can be incorrect. Please verify the accuracy of crucial factual information.</p>
-		</footer>
+		</footer-->
 	</div>
 
 </template>
